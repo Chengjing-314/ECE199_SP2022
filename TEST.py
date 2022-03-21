129ecefaa709d6c016bf1f3683ed5d67310652f8
cj <cyuan@ucsd.edu>
2022-03-21 16:54:31 -0700

    removed duck

--- TEST.py
+++ TEST.py
@@ -16,8 +16,9 @@ planeId = p.loadURDF("plane.urdf")
 pandaUID = p.loadURDF("franka_panda/panda.urdf", useFixedBase = True)
 trayUID = p.loadURDF("tray/traybox.urdf", basePosition=[0.65, 0, 0])
 plateUID = p.loadURDF("data/dinnerware/plate.urdf", basePosition = [0.7, 0, 0.01])
-cupUID = p.loadURDF("data/dinnerware/cup/cup_small.urdf", basePosition = [0.85, 0.1, 0.01])
-duckUID = p.loadURDF("data/duck/duck_vhacd.urdf", basePosition = [0.55, -0.1, 0.01])
+cup1UID = p.loadURDF("data/dinnerware/cup/cup_small.urdf", basePosition = [0.85, 0.1, 0.01])
+cup2UID = p.loadURDF("data/dinnerware/cup/cup_small.urdf", basePosition = [0.55, -0.1, 0.02])
+
 
 
 viewMatrix = p.computeViewMatrix(
@@ -31,7 +32,7 @@ projectionMatrix = p.computeProjectionMatrixFOV(
     nearVal=0.1,
     farVal=5.1)
 
-width, height, rgbImg, depthImg, segImg = p.getCameraImage(
+width, height, rgbImg, depthImg, segImg = p.getCameraImage( # img are numpy array
     width=1024, 
     height=1024,
     viewMatrix=viewMatrix,
