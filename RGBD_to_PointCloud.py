7106fb8f2b5a7928cbf627c21be7acee90536d69
cj <cyuan@ucsd.edu>
2022-03-22 23:09:04 -0700

    update

--- RGBD_to_PointCloud.py
+++ RGBD_to_PointCloud.py
@@ -19,10 +19,11 @@ trayUID = p.loadURDF("tray/traybox.urdf", basePosition=[0.65, 0, 0])
 plateUID = p.loadURDF("data/dinnerware/plate.urdf", basePosition = [0.7, 0, 0.01])
 cup1UID = p.loadURDF("data/dinnerware/cup/cup_small.urdf", basePosition = [0.85, 0.1, 0.01])
 cup2UID = p.loadURDF("data/dinnerware/cup/cup_small.urdf", basePosition = [0.55, -0.1, 0.02])
-duck = p.loadURDF("data/duck/duck_vhacd.urdf", basePosition = [0.5,0.0,0], baseOrientation = [0,0,1,0])
+duck = p.loadURDF("data/duck/duck_vhacd.urdf", basePosition = [0.5,0,0.02], baseOrientation = [0,10,0,0])
 
 
 IMG_LEN = 1024
+FOV = 60
 
 
 viewMatrix = p.computeViewMatrix(
@@ -31,7 +32,7 @@ viewMatrix = p.computeViewMatrix(
     cameraUpVector=[0, 0, 1])
 
 projectionMatrix = p.computeProjectionMatrixFOV(
-    fov=60.0,
+    fov=FOV,
     aspect=1.0,
     nearVal=0.1,
     farVal=5.1)
@@ -44,7 +45,7 @@ width, height, rgbImg, depthImg, segImg = p.getCameraImage( # img are numpy arra
 
 
 
-f = 1 / 2 * np.tan(30) 
+f = (IMG_LEN // 2) * 1 / (np.deg2rad(2 * np.tan(FOV/2)))
 
 #FIXME
 intrin = np.array([[f, 0, IMG_LEN/2 + 0.5], # -1
@@ -59,8 +60,7 @@ extrin = np.array(list(viewMatrix)).reshape((4,4))
 
 rgbImg = rgbImg[:,:,:3]
 
-intrinsic = o3d.camera.PinholeCameraIntrinsic()
-intrinsic.intrinsic_matrix = intrin
+intrinsic = o3d.camera.PinholeCameraIntrinsic(IMG_LEN, IMG_LEN, f, f, IMG_LEN/2 + 0.5, IMG_LEN/2 + 0.5)
 cam = o3d.camera.PinholeCameraParameters()
 cam.intrinsic = intrinsic
 cam.extrinsic = extrin
